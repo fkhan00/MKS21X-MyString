e09694051f732e987578f333c4e7cbeb161e4b8e
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-11-22 12:42:40 -0500

    finished methods

--- MyString.java
+++ MyString.java
@@ -1,15 +1,68 @@
-import java.util.stream.IntStream;
 public class MyString implements CharSequence, Comparable<CharSequence>
 {
   private char[] data;
   public MyString(CharSequence s)
   {
-    data = new s.length();
-    data[0] = s.charAt(0);
+    // initializing data
+    data = new char[s.length()];
+    for(int i = 0; i < s.length(); i++)
+    {
+      //filling data with... data from parameter
+      data[i] = s.charAt(i);
+    }
   }
+
   public char charAt(int index)
   {
     return data[index];
+  }
+
+  public int length()
+  {
+    return data.length;
+  }
+
+  public CharSequence subSequence(int begin, int end)
+  {
+    // starting with empty String
+    String output = "";
+    for(int i = begin; i < end; i++)
+    {
+      //concantenate each element within this sublist to output
+      output += data[i];
+    }
+    return output;
+  }
+
+  public String toString()
+  {
+    //empty string
+    String output = "";
+    for(int i = 0; i < data.length; i++)
+    {
+      //contanetate each element
+      output += data[i];
+    }
+    return output;
+  }
+
+  public int compareTo(CharSequence power)
+  {
+    if(power.length() != this.length())
+    {
+      // check for out of bound errors and inequality
+      return 1;
+    }
+    for(int i = 0; i < power.length(); i++)
+    {
+      //inequality
+      if(data[i] != this.data[i])
+      {
+        return 1;
+      }
+    }
+    // if it gets this far they must be equal
+    return 0;
   }
 
 }
