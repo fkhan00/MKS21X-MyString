46cdbe3782275eb8d7211e2308b4eee86b4e1c82
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-11-22 13:08:23 -0500

    finished driver code works

--- Driver.java
+++ Driver.java
@@ -24,36 +24,38 @@ public class Driver
     System.out.println("\ntesting subsequence");
     System.out.print("expected hel");
     System.out.println(" recieved: " + testCase.subSequence(0, 3));
-    System.out.println("expected: index out of bounds error caught");
+    System.out.println("expected: index out of bounds error upper");
     try
     {
     System.out.println(" recieved: " + testCase.subSequence(1000, 100000));
   }
   catch(IndexOutOfBoundsException e)
   {
-    System.out.println(" recieved: index out of bounds error caught");
+    System.out.println("recieved: index out of bounds error upper");
   }
-  System.out.println("expected: index out of bounds error caught");
+  System.out.println("expected: index out of bounds error end < begin");
   try
   {
-    System.out.println(" recieved: " + testCase.subSequence(10, 6));
+    System.out.println("recieved: " + testCase.subSequence(10, 6));
   }
   catch(IndexOutOfBoundsException e)
   {
-    System.out.println(" recieved: index out of bounds error caught");
+    System.out.println("recieved: index out of bounds error end < begin");
   }
-  System.out.println("expected: index out of bounds error caught");
+  System.out.println("expected: index out of bounds error lower");
   try
   {
-  System.out.println(" recieved: " + testCase.subSequence(-3, -2));
+  System.out.println("recieved: " + testCase.subSequence(-3, -2));
 }
 catch(IndexOutOfBoundsException e)
 {
-  System.out.print(" recieved: index out of bounds error caught");
+  System.out.print("recieved: index out of bounds error lower\n");
 }
 
     System.out.println("\ntesting compareTo");
     System.out.print("expected: 1");
     System.out.println(" recieved: " + testCase.compareTo("hellos"));
+    System.out.print("expected: 0");
+    System.out.println(" recieved " + testCase.compareTo("hello"));
   }
 }
