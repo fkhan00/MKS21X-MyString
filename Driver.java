4d1f8ba3f73f63a439cadb8504165163c2e2fbe1
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2018-11-22 13:01:18 -0500

    created driver

---
+++ Driver.java
@@ -0,0 +1,59 @@
+public class Driver
+{
+  public static void main(String[] args)
+  {
+    MyString testCase = new MyString("hello");
+    System.out.println("testing Constructor");
+    System.out.print("expected: hello");
+    System.out.println(" recieved: " + testCase.toString());
+    System.out.println("\ntesting length method");
+    System.out.print("expected: 5");
+    System.out.println(" recieved " + testCase.length());
+    System.out.println("\ntesting charAt method");
+    System.out.print("expected: h");
+    System.out.println(" recieved: " +  testCase.charAt(0));
+    System.out.println("expected: index out bounds error caught");
+    try{
+    System.out.println(" revieved: " + testCase.charAt(1000));
+  }
+  catch(IndexOutOfBoundsException e)
+  {
+    System.out.print("recieved : index out of bounds error caught");
+    System.out.println();
+  }
+    System.out.println("\ntesting subsequence");
+    System.out.print("expected hel");
+    System.out.println(" recieved: " + testCase.subSequence(0, 3));
+    System.out.println("expected: index out of bounds error caught");
+    try
+    {
+    System.out.println(" recieved: " + testCase.subSequence(1000, 100000));
+  }
+  catch(IndexOutOfBoundsException e)
+  {
+    System.out.println(" recieved: index out of bounds error caught");
+  }
+  System.out.println("expected: index out of bounds error caught");
+  try
+  {
+    System.out.println(" recieved: " + testCase.subSequence(10, 6));
+  }
+  catch(IndexOutOfBoundsException e)
+  {
+    System.out.println(" recieved: index out of bounds error caught");
+  }
+  System.out.println("expected: index out of bounds error caught");
+  try
+  {
+  System.out.println(" recieved: " + testCase.subSequence(-3, -2));
+}
+catch(IndexOutOfBoundsException e)
+{
+  System.out.print(" recieved: index out of bounds error caught");
+}
+
+    System.out.println("\ntesting compareTo");
+    System.out.print("expected: 1");
+    System.out.println(" recieved: " + testCase.compareTo("hellos"));
+  }
+}
